021fe900359e6d37abef4319322137afb9157600
lorevis92 <lorenzo.visconti@hotmail.it>
2023-08-04 10:54:21 +0200

    Settaggio progetto comunicante con db postgresql "GestioneDispositiviAziendali" e con il port 3001

---
+++ src/main/java/Epicode/ProgettoSettimana06/ProgettoSettimana06Application.java
@@ -0,0 +1,13 @@
+package Epicode.ProgettoSettimana06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana06Application.class, args);
+	}
+
+}
---
+++ src/test/java/Epicode/ProgettoSettimana06/ProgettoSettimana06ApplicationTests.java
@@ -0,0 +1,13 @@
+package Epicode.ProgettoSettimana06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimana06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
